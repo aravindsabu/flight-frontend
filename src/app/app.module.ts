b11f8579bbf6bc8331121897782de7b2d7379c9e
aravindsabu <aravindsabu2001@gmail.com>
2022-10-19 16:47:10 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddflightComponent } from './addflight/addflight.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+const appRoutes:Routes=[
+  {
+    path:"",component:AddflightComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddflightComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
